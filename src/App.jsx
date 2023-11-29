e32e5011689cbe37d7aa21b59aebf931d43a10a7
clebsantos404 <clebsondantas404@gmail.com>
2023-11-29 07:55:35 -0300

    format

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import css from 'index.css';
+
+function App() {
+  return (
+    <div>
+      <nav id="navbar">
+        <h2>Portfólio</h2>
+        <a href="">Sobre mim</a>
+        <a href="">Projetos</a>
+        <a href="">Serviços</a>
+        <a href="">Minhas skills</a>
+      </nav>
+
+    </div>
+    );
+}
+
+export default App;
